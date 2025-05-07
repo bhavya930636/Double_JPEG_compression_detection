c6c5bc95438fbd546a1467f6d82e68d53b85722c
chetaniitbhilai <chetan@iitbhilai.ac.in>
2025-05-07 20:17:05 +0530

    updated_code

--- convert2.py
+++ convert2.py
@@ -1,27 +1,3 @@
-# import numpy as np
-# from scipy.io import loadmat
-# import os
-# import sys
-
-# def convert_mat_to_npz(mat_file_path):
-#     if not os.path.exists(mat_file_path):
-#         print(f"Error: File '{mat_file_path}' not found.")
-#         return
-
-#     mat_data = loadmat(mat_file_path)
-#     filtered_data = {k: v for k, v in mat_data.items() if not k.startswith('__')}
-#     npz_file_path = os.path.splitext(mat_file_path)[0] + '.npz'
-#     np.savez(npz_file_path, **filtered_data)
-#     print(f"Successfully saved: {npz_file_path}")
-
-# if __name__ == "__main__":
-#     if len(sys.argv) != 2:
-#         print("Usage: python mat_to_npz.py <file.mat>")
-#         sys.exit(1)
-    
-#     mat_file = sys.argv[1]
-#     convert_mat_to_npz(mat_file)
-
 import numpy as np
 from scipy.io import loadmat
 import os
